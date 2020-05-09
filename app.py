7b761a45116213f6c7cccc69fed37c7ad2bf9227
desire <desire@localhost.localdomain>
2020-05-09 22:25:31 +0200

    update app.py

--- app.py
+++ app.py
@@ -17,25 +17,25 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    #int_features = [int(x) for x in request.form.values()]
-    #final_features = [np.array(int_features)]
-    #result = model.predict(final_features)
-    #output = round(result[0], 2)
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    result = model.predict(final_features)
+    output = round(result[0], 2)
 
     # get data
     #data = request.get_json(force=True)
 
     # convert data into dataframe
-    data.update((x, [y]) for x, y in data.items())
+    #data.update((x, [y]) for x, y in data.items())
     ## you need to convert the JSON data that you get from the request to a data structure that the model can use to make a prediction.
     ## Here a pandas dataframe object
-    data_df = pd.DataFrame.from_dict(data)
+    #data_df = pd.DataFrame.from_dict(data)
 
     # predictions
-    result = model.predict(data_df)
+    #result = model.predict(data_df)
 
     # send back to browser
-    output = {'results': int(result[0])}
+    #output = {'results': int(result[0])}
 
     # return data
     #return jsonify(results=output)
@@ -46,14 +46,13 @@ def results():
 
     # get data
     data = request.get_json(force=True)
-    result = model.predict([np.array(pd.DataFrame.from_dict(data.values()))])
+    result = model.predict([np.array(list(data.values()))])
 
     # return data
     #return jsonify(results=output)
 
     output = result[0]
-    #return jsonify(output)
-    #return jsonify(results=output)
+    return jsonify(output)
 
 if __name__ == '__main__':
     app.run(port = 7070, debug=True)
