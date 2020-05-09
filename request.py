6b343111bed723dcd082ad5ff272287b0f9a55ac
desire <desire@localhost.localdomain>
2020-05-09 21:28:43 +0200

    update app.py

--- request.py
+++ request.py
@@ -1,8 +1,7 @@
 import requests
 import json
 
-#url = 'http://localhost:5000/results'
-heroku_url = https://predictivapp.herokuapp.com/
+heroku_url = https://predictivapp.herokuapp.com/results
 send_request = requests.post(heroku_url,json={'Pclass': 3, 'Age': 2, 'SibSp': 1, 'Fare': 50})
 
 print(send_request.json())
