ea25195bd4cb8b5f2b38096b535b09bc6baeab79
desire <desire@localhost.localdomain>
2020-05-10 12:41:07 +0200

    updated request.py

--- request.py
+++ request.py
@@ -2,6 +2,6 @@ import requests
 import json
 
 url = 'http://localhost:7070/results'
-send_request = requests.post(heroku_url,json={'Pclass': 3, 'Age': 2, 'SibSp': 1, 'Fare': 50})
+send_request = requests.post(url,json={'Pclass': 3, 'Age': 2, 'SibSp': 1, 'Fare': 50})
 
 print(send_request.json())
